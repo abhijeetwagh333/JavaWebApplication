80c560244c2d593920b45d1e2bc83adc38fd78f5
Abhijeet Wagh <waghabhijeet333@gmail.com>
2022-06-21 22:49:02 +0530

    Adding logger

--- src/main/java/com/app/controller/LoginController.java
+++ src/main/java/com/app/controller/LoginController.java
@@ -1,14 +1,17 @@
 package com.app.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
-	
-	
+	Logger logger = LoggerFactory.getLogger(LoginController.class);
+
 	@GetMapping("/getMessage")
 	public String getMessage() {
+		logger.info(" Loggin controller getMessage method ");
 		return "this is docker intgeration demo";
 	}
 
